77d299c1dc3db26e5d4d77dbb1ff46b84c188e48
livaen <darius2424@gmail.com>
2018-07-23 22:37:54 +0200

    Format Code

--- Chat.API/Startup.cs
+++ Chat.API/Startup.cs
@@ -58,12 +58,10 @@ namespace Chat.API
                         {
                             var accessToken = context.Request.Query["access_token"];
 
-                            // If the request is for our hub...
                             var path = context.HttpContext.Request.Path;
                             if (!string.IsNullOrEmpty(accessToken) &&
                                 (path.StartsWithSegments("/chat")))
                             {
-                                // Read the token out of the query string
                                 context.Token = accessToken;
                             }
                             return Task.CompletedTask;
